26a7d1115b1a8d4a285720f84650bd0da40606d4
Quentin Bahers <quentin.bahers@gmail.com>
2015-02-02 15:52:33 +0000

    Wiring in the interface

--- site/js/views/library.js
+++ site/js/views/library.js
@@ -3,9 +3,15 @@ var app = app || {};
 app.LibraryView = Backbone.View.extend({
   el: '#books',
 
+  events:{
+    'click #add':'addBook'
+  },
+
   initialize: function( initialBooks ) {
     this.collection = new app.Library( initialBooks );
     this.render();
+
+    this.listenTo( this.collection, 'add', this.renderBook );
   },
 
   // render library by rendering each book in its collection
@@ -22,5 +28,20 @@ app.LibraryView = Backbone.View.extend({
       model: item
     });
     this.$el.append( bookView.render().el );
+  },
+
+  addBook: function( e ) {
+    e.preventDefault();
+
+    var formData = {};
+
+    $( '#addBook div' ).children( 'input' ).each( function( i, el ) {
+      if( $( el ).val() != '' )
+      {
+	formData[ el.id ] = $( el ).val();
+      }
+    });
+
+    this.collection.add( new app.Book( formData ) );
   }
 });
